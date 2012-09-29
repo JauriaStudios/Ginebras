972f90b058c48236d300cbe36d3e243ab658b0ea
Jose Luis <j.l.toledano.l@gmail.com>
2012-09-29 19:30:19 +0200

    Text Box display test

--- src/textbox.h
+++ src/textbox.h
@@ -7,26 +7,6 @@
 #include "list.h"
 #include "gameUtilities.h"
 
-//#include "cursor.h"
-
-#ifndef SCREEN_WIDTH
-#define SCREEN_WIDTH  	800
-#endif
-
-#ifndef SCREEN_HEIGHT
-#define SCREEN_HEIGHT 	600
-#endif
-
-#define BUFFER_SIZE 1024
-
-#define MAP_SIZE_X 40
-#define MAP_SIZE_Y 40
-
-#ifndef TILE_SIZE
-#define TILE_SIZE 32
-
-#endif
-
 /**********************************************************
  *** STRUCTS
  **********************************************************/
@@ -34,14 +14,12 @@ typedef struct Textbox{
 	
 	SDL_Surface *background;
 	SDL_Surface *message;
-	SDL_Rect     rcDest;
-
-	TTF_Font* font;
-	SDL_Color textColor;
-	// message properties
+	SDL_Rect     rcText, rcBox;
 	
-	// box properties
-	int  x, y, boxWidth, boxHeight;
+	TTF_Font *font;
+	SDL_Color textColor;
+	
+	char *textMsg;
 	
 } Textbox;
 
@@ -50,9 +28,9 @@ typedef struct Textbox{
  *** METHODS
  **********************************************************/
 extern Textbox* TextboxConstructor(SDL_Surface *screen);
-extern void TextboxDestructor (Textbox * textbox);
-extern void TextboxLoad (Textbox * textbox);
-extern void TextboxUpdate (Textbox * textbox);
-extern void TextboxDraw (Textbox * textbox, SDL_Surface* screen);
+extern void TextboxDestructor(Textbox *textbox);
+extern void TextboxLoad(Textbox *textbox);
+extern void TextboxUpdate(Textbox *textbox);
+extern int TextboxDraw(Textbox *textbox, SDL_Surface *screen);
 
 #endif
